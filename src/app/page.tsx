367d240f1173196041cf45ffac43a70c3868efc3
Hangleang <hangleang127@gmail.com>
2024-01-01 16:38:08 +0700

    feat(setup): setup project structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 import { CreatePost } from "@/app/_components/create-post";
-import { api } from "@/trpc/server";
+import { api } from "@/services/trpc/server";
 
 export default async function Home() {
   const hello = await api.post.hello.query({ text: "from tRPC" });
